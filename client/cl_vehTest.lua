0c2e16e2196496b9642e27dcfb3a8af18719f409
Fuxlor <73135866+Fuxlor@users.noreply.github.com>
2022-10-22 15:27:29 +0200

    Update cl_vehTest.lua

--- client/cl_vehTest.lua
+++ client/cl_vehTest.lua
@@ -8,24 +8,36 @@ Citizen.CreateThread(function()
 end)
 
 RegisterCommand('test', function(source, args, rawCommand)
-    ESX.ShowAdvancedNotification('vehTest', '', 'Profite bien de tes 30 secondes de test !', 'CHAR_CARSITE', 2) -- notif début
     local veh = args[1]
     local time = 3000
-    temps = 30
+    local sorti = false
+    local temps = 30
     local lastPos = GetEntityCoords(PlayerPedId())
-    SetEntityCoords(PlayerPedId(), -963.1749, -3358.958, 13.9772) -- lieu de tp pour tester
-    RequestModel(veh)
-    while not HasModelLoaded(veh) do Citizen.Wait(10) end 
-    local pos = GetEntityCoords(PlayerPedId())
-    local vehicle = CreateVehicle(veh, pos.x, pos.y, pos.z, 90, true, false)
-    TaskWarpPedIntoVehicle(PlayerPedId(), vehicle, -1)
-    while time > 0 do
-        time = time - 0.7
-        temps = time /100
-        RageUI.Text({message='~g~'..math.floor(temps + 0.5*0).. ' ~w~secondes restantes'})
-    Wait(0)
+    if veh ~= nil then
+        RequestModel(veh)
+        while not HasModelLoaded(veh) do Citizen.Wait(10) end 
+        ESX.ShowAdvancedNotification('vehTest', '', 'Profite bien de tes 30 secondes de test !', 'CHAR_CARSITE', 2) -- notif début
+        SetEntityCoords(PlayerPedId(), -963.1749, -3358.958, 13.9772) -- lieu de tp pour tester
+        local pos = GetEntityCoords(PlayerPedId())
+        local vehicle = CreateVehicle(veh, pos.x, pos.y, pos.z, 90, true, false)
+        TaskWarpPedIntoVehicle(PlayerPedId(), vehicle, -1)
+        Wait(100)
+        while time > 0 do
+            if IsPedInAnyVehicle(PlayerPedId(), true) then
+                time = time - 0.7
+                temps = time /100
+                RageUI.Text({message='~g~'..math.floor(temps + 0.5*0).. ' ~w~secondes restantes'})
+            else
+                ESX.ShowAdvancedNotification('vehTest', '', 'Tu es sorti du véhicule !', 'CHAR_CARSITE', 2) -- notif fin
+                sorti = true
+                time = 0
+            end
+        Wait(0)
+        end
+        DeleteEntity(vehicle)
+        if sorti == false then ESX.ShowAdvancedNotification('vehTest', '', 'C\'est fini !', 'CHAR_CARSITE', 2) end -- notif fin 
+        SetEntityCoords(PlayerPedId(), lastPos)
+    else
+        ESX.ShowNotification('aucun véhicule fourni')
     end
-    DeleteEntity(vehicle)
-    ESX.ShowAdvancedNotification('vehTest', '', 'C\'est fini !', 'CHAR_CARSITE', 2) -- notif fin
-    SetEntityCoords(PlayerPedId(), lastPos)
 end, false)
